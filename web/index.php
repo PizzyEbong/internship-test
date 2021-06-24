267ad6457401e8e5f669a62f108281093d0ec445
PizzyEbong <47450744+PizzyEbong@users.noreply.github.com>
2021-06-24 09:54:52 +0100

    Add files via upload

---
+++ web/index.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Document</title>
+</head>
+<body>
+
+    <?php 
+
+        $basket = range(0, 100, 5);
+        print '<pre>';
+        print_r($basket);
+        print '</pre>';
+
+
+        $costValue = [1, 5, 10, 15, 20, 25, 30];
+        print  array_sum( $costValue)
+
+
+
+      
+
+      
+    
+    ?>
+
+
+</body>
+</html>
